db6dea407aed9ac92ebf16e0d8ec9fdc2a7a6113
Stephan <stephan.startsev@gmail.com>
2019-10-30 16:12:52 +0300

    remedy

--- firstHW/ez/main.js
+++ firstHW/ez/main.js
@@ -8,29 +8,22 @@ const sponsors = {
 
 let employersNames = employers.filter(el => {
   return el.length > 0 
-})
+}).map( el => el.toLowerCase().trim())
 
-employersNames = employersNames.map(el => {
-  return el.toLowerCase().trim();
-})
-
-function calcCash(own = 0) {
-    let total = 0;
-    own.forEach(el => {
-      return total += el
-    });
-    return total
+const calcCash = (own = 0) => {
+  return own.reduce((el, el1) => el + el1)
 }
 
 const money = calcCash(sponsors.cash);
 
-function makeBusiness(owner, director = 'Victor', money, emp) {
+const makeBusiness = (owner, director = 'Victor', money, emp) => {
     const {cash, eu, rus} = sponsors ;
-    const sumSponsors = eu.concat(rus, 'unexpected sponsor') ;
+    const sumSponsors = [...[...eu, ...rus, 'unexpected sponsor']];
     console.log(`We have a business. Owner: ${owner}, director: ${director}. Our budget: ${money}. And our employers:
-    ${emp}`);
-    console.log('And we have a sponsors: ');
-    console.log(...sumSponsors);
-    console.log(`Note. Be careful with ${eu[0]}. It's a huge risk.`);
+    ${emp}
+    And we have a sponsors:
+    ${sumSponsors}
+    Note. Be careful with ${eu[0]}. It's a huge risk.`);
 }
+
 makeBusiness(...['Sam', ,money, employersNames]);
