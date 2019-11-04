ec00fa8e667bdd303faa316eda34aeda4b78d54f
Stephan <stephan.startsev@gmail.com>
2019-11-04 23:40:59 +0300

    add precommit/check

--- thirdHW/src/index.js
+++ thirdHW/src/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import './styles.scss';
 
-ReactDOM.render(<h1>Hello world</h1>, document.getElementById("app"));
+ReactDOM.render(<h1>Hello world!!</h1>, document.getElementById("app"));
