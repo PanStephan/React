121a4baf3c30d04a592d46741f08b28cecf0c87f
Stephan <stephan.startsev@gmail.com>
2019-11-09 11:13:51 +0300

    refactoring

--- postsApp/src/components/postList/postList.js
+++ postsApp/src/components/postList/postList.js
@@ -7,15 +7,13 @@ import { ListGroup, ListGroupItem  } from 'reactstrap';
 export default class PostList extends React.Component {
 
   render() {
-    const {posts, onDelete, onToggleImportant, onToggleLiked, onUpdateLabel, createNewData} = this.props;
+    const {posts, onDelete, onUpdateLabel, createNewData} = this.props;
     const elements = posts.map(item => {
       const{id, ...itemProps} = item;
       return (
         <ListGroupItem key={id} className='list-group-item'>
           <PostListItem {...item}
           onDelete={() => onDelete(id)}
-          // onToggleImportant={() => onToggleImportant(id)}
-          // onToggleLiked={() => onToggleLiked(id)}
           createNewData={createNewData}
           onUpdateLabel={onUpdateLabel}/>
         </ListGroupItem>
